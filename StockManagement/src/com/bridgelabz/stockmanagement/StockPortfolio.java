dbf4b4e4ab333a7bbced641f44f9ca09d42908ca
namancg <naman.c@perfios.com>
2021-09-05 21:18:49 +0530

    ADDED: buy and sell from stock with details

--- StockManagement/src/com/bridgelabz/stockmanagement/StockPortfolio.java
+++ StockManagement/src/com/bridgelabz/stockmanagement/StockPortfolio.java
@@ -2,34 +2,13 @@ package com.bridgelabz.stockmanagement;
 import java.util.ArrayList;
 public class StockPortfolio {
 public static void main(String[] args) {
-		ArrayList<Stock> stockPortfolio = new ArrayList<>();
-		Stock temp = new Stock();
-		temp.setStockName("SBI");
-		temp.setNumOfShares(10);
-		temp.setSharePrice(1200);
-		stockPortfolio.add(temp);
-		temp = new Stock();
-		temp.setStockName("Tesla");
-		temp.setNumOfShares(18);
-		temp.setSharePrice(2000);
-		stockPortfolio.add(temp);
-		temp = new Stock();
-		temp.setStockName("OnePlus");
-		temp.setNumOfShares(45);
-		temp.setSharePrice(100);
-		stockPortfolio.add(temp);
-		double valueOfStock , total=0;
-		for(Stock stock : stockPortfolio) {
-			System.out.println(stock);
-			valueOfStock=calculateStockValue(stock);
-			total=total+valueOfStock;
-			System.out.println("Value of Stock is "+valueOfStock);
-		}
-		System.out.println("TOTAL VALUE: "+total);
-	}
-
-	private static double calculateStockValue(Stock stock) {
-		double temp=stock.getNumOfShares() * stock.getSharePrice();
-		return temp;
-	}
+	StockAccount stockAccount = new StockAccount();
+	System.out.println("Initial: ");
+	stockAccount.printReport();
+	System.out.println();
+	stockAccount.buy(10, "SBI");
+	stockAccount.sell(3, "OnePlus");
+	System.out.println("Report: ");
+	stockAccount.printReport();
+}
 }
